e60a3233d6dedf1d5b7cf805ea16c00cf40fe4f4
leerussellhiggitt <lee.higgitt@gmail.com>
2018-12-06 11:50:03 +0000

    Updated client secret

--- src/twitch_client/options.js
+++ src/twitch_client/options.js
@@ -1,5 +1,5 @@
 export const twitchOptions = {
     clientId: "fs4r46yzv1ng0aj55zbqid4op65b2v",
-    clientSecret: "63opjsw71v21861efaic9bv4qmhr8g",
+    clientSecret: "mw3uvdz8eg5g0cuf0j13v6iw1ta1to",
     storageKey: "TWITCH_ACCESS_TOKEN"
 };
