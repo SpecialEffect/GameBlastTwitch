8c46e906c3a24b64c2c1dbfd1c858de84509dcc9
leerussellhiggitt <lee.higgitt@gmail.com>
2018-12-06 10:22:52 +0000

    Fixed box sizing. Fixed padding on mobile. Fixed live indicator. Fixed sorting by live. Changed page controls so that tappable area is much larger

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 import { getTwitchUsers, getTwitchStreams, getGames } from "./twitch_client/client";
 import { render } from "./render/carousel";
 
-getTwitchUsers(["xgladd", "kinggothalion", "professorbroman", "dakotaz", "symfuhny"])
-    .then(users => {
-    getTwitchStreams(users.map(x => x.login))
-        .then(streams => {
-        getGames(streams.map(x => x.game_id))
-            .then(games => {
-                render(users, streams, games);
-        })
-    });
-});
+const userNames = _getTwitchUserNames();
+
+if (userNames !== undefined && userNames !== null && typeof userNames[Symbol.iterator] === "function" && userNames.length > 0) {
+
+    render(null, null, null);
+
+    getTwitchUsers(userNames)
+        .then(users => {
+            getTwitchStreams(users.map(x => x.login))
+                .then(streams => {
+                    getGames(streams.map(x => x.game_id))
+                        .then(games => {
+                            render(users, streams, games);
+                        })
+                });
+        });
+}
+
+function _getTwitchUserNames() {
+    const element = document.querySelector(".twitch-usernames");
+    if (element === undefined || element === null) {
+        return [];
+    }
+
+    return element.innerText.split(",").map(x => x.trim()).filter(x => x);
+}
--- src/render/carousel.js
+++ src/render/carousel.js
@@ -3,31 +3,37 @@ import { createElement } from "./helper";
 const tilesPerPage = 3;
 
 export const render = (users, streams, games) => {
-    const pagedUsers = [].concat.apply([], users.map((e, i) => i % tilesPerPage ? [] : [users.slice(i, i + tilesPerPage)]));
 
-    const pages = createElement("div", ["twitch-carousel__pages"], null, _renderPages(pagedUsers, streams, games)); 
+    const carousel = createElement("div", ["twitch-carousel"]);
 
-    const carousel = createElement("div", ["twitch-carousel"], null, [
-        createElement("h2", ["twitch-carousel__title"], null, [ 
-            createElement("span", null, null, null, "Streaming") 
-        ], "Now"),
-        createElement("div", ["twitch-carousel__padding"], null, [
-            pages
-        ]),
-        createElement("div", ["twitch-carousel__controls"], null, _renderPagingControls(pagedUsers, pages))
-    ]);
 
-    const element = document.getElementById("carousel");
-    if(element !== null) {
-        element.appendChild(carousel);
+    if (users !== null && streams !== null && games !== null) {
+        const sorted = users.sort((a,b) => _isUserLive(a, streams) ? -1 : 1);
+
+        const pagedUsers = [].concat.apply([], sorted.map((e, i) => i % tilesPerPage ? [] : [sorted.slice(i, i + tilesPerPage)]));
+        const pages = createElement("div", ["twitch-carousel__pages"], null, _renderPages(pagedUsers, streams, games));
+        carousel.appendChild(createElement("h2", ["twitch-carousel__title"], null, [createElement("span", null, null, null, "Streaming")], "Now"));
+        carousel.appendChild(createElement("div", ["twitch-carousel__padding"], null, [pages]));
+        carousel.appendChild(createElement("div", ["twitch-carousel__controls"], null, _renderPagingControls(pagedUsers, pages)));
     }
 
+    _insertElement(carousel)
+}
+
+function _insertElement(carousel) {
+    const element = document.getElementById("carousel");
+    if (element !== null) {
+        while (element.firstElementChild) {
+            element.firstElementChild.remove();
+        }
+        element.appendChild(carousel);
+    }
 }
 
 function _renderPages(pagedUsers, streams, games) {
-    
+
     const pages = [];
-    for(let page of pagedUsers) {
+    for (let page of pagedUsers) {
         pages.push(createElement("div", ["twitch-carousel__page"], null, page.map(user => _renderCard(user, streams, games))))
     }
 
@@ -36,14 +42,14 @@ function _renderPages(pagedUsers, streams, games) {
 
 function _renderCard(user, streams, games) {
 
-    const stream = streams.filter(x => x.user_id === user.id)[0];
+    const stream = _getStreamForUser(user, streams);
 
     let imageUrl = null;
     let isLive = false;
     let gameName = "";
     let viewerCount = 0;
 
-    if(stream === undefined || stream === null) {
+    if (stream === undefined || stream === null) {
         imageUrl = user.offline_image_url;
     } else {
         isLive = true;
@@ -52,7 +58,8 @@ function _renderCard(user, streams, games) {
         gameName = games.filter(x => x.id === stream.game_id)[0].name;
     }
 
-    return createElement("div", ["twitch-carousel__card"], null, [
+
+    const card = createElement("div", ["twitch-carousel__card"], null, [
         createElement("img", ["twitch-carousel__card__image"], [
             { name: "src", value: imageUrl }
         ]),
@@ -63,20 +70,25 @@ function _renderCard(user, streams, games) {
                 createElement("p", null, null, null, viewerCount + " viewers")
             ])
         ]),
-        createElement("div", ["twitch-carousel__card__views"], null, [
+    ]);
+
+    if (isLive) {
+        card.appendChild(createElement("div", ["twitch-carousel__card__views"], null, [
             createElement("div", ["twitch-carousel__card__views__indicator"]),
             createElement("span", null, null, null, "Live")
 
-        ])
-    ]);
+        ]));
+    }
+
+    return card;
 }
 
 function _renderPagingControls(pagedUsers, pages) {
     const controls = [];
-    for(let i = 0; i < pagedUsers.length; i ++) {
+    for (let i = 0; i < pagedUsers.length; i++) {
 
         const classes = ["twitch-carousel__control"];
-        if(i === 0) classes.push("active");
+        if (i === 0) classes.push("active");
 
         const control = createElement("div", classes);
         controls.push(control);
@@ -85,6 +97,21 @@ function _renderPagingControls(pagedUsers, pages) {
             controls.forEach(x => x.classList.remove("active"));
             controls[i].classList.add("active");
         });
-    }   
+    }
     return controls;
+}
+
+function _isUserLive(user, streams) {
+    return _getStreamForUser(user, streams) !== null;
+}
+
+function _getStreamForUser(user, streams) {
+    const stream =  streams.filter(x => x.user_id === user.id)[0];
+    if(stream !== undefined && stream !== null) {
+        console.debug(`${user.display_name} has stream with id ${stream.id}`);
+        return stream;
+    } else {
+        console.debug(`${user.display_name} is not streaming`)
+        return null;
+    }
 }
